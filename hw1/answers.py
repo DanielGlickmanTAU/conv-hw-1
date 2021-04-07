496982de1d9c36a3efae97cd50cb8ee139702f91
ciprutdavid <davidciprut@mail.tau.ac.il>
2021-04-08 02:42:02 +0300

    answer

--- hw1/answers.py
+++ hw1/answers.py
@@ -9,16 +9,9 @@ math (delimited with $$).
 # Part 2 answers
 
 part2_q1 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+**Your answer: Increasing the value of k will not necessarily increase the accuracy. 
+Let's look at the output graph above. The best accuracy is achieved at k=15. But when k=50
+the accuracy decreases**
 """
 # ==============
 
@@ -28,15 +21,6 @@ An equation: $e^{i\pi} -1 = 0$
 part3_q1 = r"""
 ** The delta forces a distances of the samples from the hyperplane W creates. 
 The same hyperplane will be reached, but scaled by a constant scalar, which will scale the weights by the same constant**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
 """
 
 part3_q2 = r"""
@@ -48,15 +32,6 @@ The distance analogy is similar with knn but different in that:
 1) different distance metric is used(dot product vs euclidean)**
 2) knn is optimized/punished for distance with the top k examples while the linear classifier measures
 the average distance from all the dataset
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
 """
 
 part3_q3 = r"""
@@ -71,18 +46,7 @@ part3_q3 = r"""
  
  2) The model is slightly overfitted. We can see that by the fact that the train accuracy is higher
  then the validation's. That fact remains stable from about epoch 10 
- 
- 
 **
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
 """
 
 # ==============
@@ -91,29 +55,17 @@ An equation: $e^{i\pi} -1 = 0$
 # Part 4 answers
 
 part4_q1 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+** **
 
 """
 
 part4_q2 = r"""
 **Your answer:**
+1.
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
+2. 
 """
 
 # ==============
